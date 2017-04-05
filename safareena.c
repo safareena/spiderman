c43cb9e2d892d30ea6c26279a3da28dc00792834
safareena <safareena21@gmail.com>
2017-04-05 10:18:54 +0530

    Update safareena.c

--- safareena.c
+++ safareena.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 void main()
 {
-   int a,b,sum;
+   int a,b,sum,mul;
    printf("helloworld");
    printf("enter 1st no");
    scanf("%d",&a);
    printf("enter 2 nd no");
    scanf("%d",&b);
    sum=a+b;
-   printf("the sum is ",sum);
-   mul
+   printf("the sum is %d",sum);
+   mul=a*b;
+   printf("the product is %d",mul);
 } 
 
    
